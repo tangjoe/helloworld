7c87a04c5ec54617131950b88bd7b75c8768ccbc
Joe Tang <tangwcjoe@gmail.com>
2020-11-30 10:37:47 +0800

    changes

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.*;
+
 @RestController
 @EnableAutoConfiguration
 public class Application {
@@ -24,8 +26,6 @@ float x,y,z;
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Application.class, args);
+        System.out.println("End of code here ...");
     }
-
-    System.out.println("End of code here ...");
-
 }
